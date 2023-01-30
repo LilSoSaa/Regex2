4a95ee2084df4693e7f4a84b82c236e791b0098a
giova <giovannitopor@gmail.com>
2023-01-30 19:52:46 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String string1 = "tup tuup tuuup tuuuup";
+        System.out.println(string1.replaceAll("[u]{2,3}","X"));
+    }
+}
